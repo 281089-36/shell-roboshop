dc243d0bdfa31330cfa6c7002604215717d6921b
msivakumarreddy <info@joindevops.com>
2025-09-14 13:33:57 +0530

    Hello world started

--- shipping.sh
+++ shipping.sh
@@ -80,7 +80,7 @@ VALIDATE $? "Starting Shipping"
 dnf install mysql -y  &>>$LOG_FILE
 VALIDATE $? "Install MySQL"
 
-mysql -h mysql.thulasi.fun -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
+mysql -h mysql.thulasi.fun -uroot -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
     mysql -h mysql.thulasi.fun -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
